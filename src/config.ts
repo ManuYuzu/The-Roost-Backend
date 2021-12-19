8a0e2ea5dbb9b9d625da11d66b6232fb8c255344
Manu Glez <manuaglez@gmail.com>
2021-12-19 13:14:10 +0000

    Database connected. Remember to setup .env before use it

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,5 @@ export default {
 	PORT: process.env.PORT || 3000,
 	SECRET: process.env.SECRET || 'super-secret-here-test',
 	MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost',
-	MONGO_DB: process.env.MONGO_DB || 'API-TEST-CRM-SERVICE'
+	MONGO_DB: process.env.MONGO_DB || 'the-roost-service'
 }
